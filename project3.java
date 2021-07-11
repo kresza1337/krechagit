d5f29c164780212ba0e00576fd8b98c54b230589
kresza1337 <58442095+kresza1337@users.noreply.github.com>
2021-07-11 17:48:34 +0200

    Lesson 3 - Operatory arytmetyczne1

---
+++ project3.java
@@ -0,0 +1,29 @@
+public class project3 {
+    public static void main(String[] args){
+        int a = 15;
+        int b = 10;
+        int wynik = a + b;
+         System.out.println("Wynik: " + wynik);
+        int c = 35;
+        int d = 8;
+        int wynik2 = c - d;
+        System.out.println("Wynik: " + wynik2);
+        int e = 35;
+        int f = 8;
+        int wynik3 = e * f;
+        System.out.println("Wynik: " + wynik3);
+        int g = 35;
+        int h = 8;
+        double wynik4 = (double)g / h;
+        System.out.println("Wynik: " + wynik4);
+        int i = 35;
+        int j = 8;
+        int wynik5 = i % j;
+        System.out.println("Reszta: " + wynik5);
+        int k = 35;
+        int l = 8;
+        int wynik6 = 2 + 2 * 2;
+        System.out.println("Wynik: " + wynik6);
+
+    }
+}
