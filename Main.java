5d1adc258a525aec252d565fed7063dbb745dd65
kresza1337 <58442095+kresza1337@users.noreply.github.com>
2021-07-12 15:17:12 +0200

    Lesson 7 - Instrukcje Switch

--- Main.java
+++ Main.java
@@ -1,37 +1,33 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-            String name;
-                System.out.println("Your name: ");
-                name = scanner.nextLine();
-                System.out.println("Hello " + name);
-            int age;
-                System.out.println("Your age : ");
-                age = scanner.nextInt();
-                    if( age >= 18 ) {
-                        System.out.println("Hi " + age + " years old bro!");
-                        int money;
-                        System.out.println("Enter your monthly salary: ");
-                        money = scanner.nextInt();
-                            if( money > 2800 ){
-                                System.out.println("You are rich");
-                            }
-                            else if( money == 2800 ){
-                                System.out.println("You are not rich or poor");
-                            }
-                            else{
-                                System.out.println("Hello poor man");
-                            }
-                    }
-                    else{
-                        System.out.println("Hi " + age + " years old child!");
-                    }
+        System.out.println("Wybierz cyfre pory roku: ");
+        int poraRoku;
+        poraRoku = scanner.nextInt();
+        // switch (do jakiej zmiennej)
+        switch(poraRoku){
+                    //dwukropek!
+            case 1:
+                System.out.println("Wiosna");
+                break; //skok - blokuje dalszy tok
+            case 2:
+                System.out.println("Lato");
+                break;
+            case 3:
+                System.out.println("Jesień");
+                break;
+            case 4:
+                System.out.println("Zima");
+                break;
+                // wartosc nieprzewidziana
+            default:
+                System.out.println("Nie ma takiej pory roku, wpisz cyfre od 1 - 4");
+                break;
+
+
         }
+
     }
-
-
-
-
-
+}
