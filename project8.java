b37a7749967ff7fcf370fc4541ab3e7d67af3de8
kresza1337 <58442095+kresza1337@users.noreply.github.com>
2021-07-24 13:38:46 +0200

    Lesson 8 - Pętle

---
+++ project8.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class project8 {
+
+    public static void main(String[] args) {
+     //   int i = -5;
+     //   while (i > 0){
+     //       System.out.println(i);
+     //       i--;
+     //   }
+     //   do{
+     //       System.out.println(i);
+     //       i--;
+     //   } while(i > 0);
+
+
+
+        for(int i = 5; i > 0; i--){
+           System.out.println(i);
+        }
+
+    }
+}
